c83cab03af76e36d9cbcf24552606d09126612d9
Josh Luria <josh.luria@gmail.com>
2017-11-07 15:35:30 -0500

    Use nunjucks to render html

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const app = express();
 const chalk = require('chalk');
 const log = console.log;
 const nunjucks = require('nunjucks');
-var env = nunjucks.configure('views', {noCache: true});
+app.set('view engine', 'html');
+app.engine('html', nunjucks.render);
+nunjucks.configure('views', {noCache: true});
 var locals = {
   title: 'Fantasy Characters',
   people: [
@@ -11,10 +13,7 @@ var locals = {
     {name: 'Frodo'},
     {name: 'Hermione'}
   ]
-}
-var index = env.render('index.html', locals, function(err, output){
-  console.log(output);
-});
+};
 
 app.use(function (req, res, next) {
   log(chalk.white(req.method, req.url));
@@ -26,5 +25,5 @@ app.listen(3000,() => {
 });
 
 app.get('/', (req, res) => {
-  res.send(index);
+  res.render('index', locals);
 });
