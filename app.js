348b2b8ab6c94fd1a59464b56b870c7055e67f54
Josh Luria <josh.luria@gmail.com>
2017-11-07 14:39:47 -0500

    Handle errors rendering index

--- app.js
+++ app.js
@@ -12,7 +12,9 @@ var locals = {
     {name: 'Hermione'}
   ]
 }
-var index = env.render('index.html', locals);
+var index = env.render('index.html', locals, function(err, output){
+  console.log(output);
+});
 
 app.use(function (req, res, next) {
   log(chalk.white(req.method, req.url));
@@ -20,19 +22,9 @@ app.use(function (req, res, next) {
 });
 
 app.listen(3000,() => {
-	log(chalk.yellow('listening on port 3000'));
+  log(chalk.yellow('listening on port 3000'));
 });
 
 app.get('/', (req, res) => {
-	res.send(index);
-	/*res.send(`
-    <html>
-		<head>
-			<title>Homepage</title>
-		</head>
-		<body>
-			<h1>Welcome!</h1>
-		</body>
-    </html>
-	`)*/
+  res.send(index);
 });
