8fdabffb764308db8c9da62889eb58821e1abcf5
Josh Luria <josh.luria@gmail.com>
2017-11-07 14:33:57 -0500

    Add variables to index page

--- app.js
+++ app.js
@@ -4,7 +4,15 @@ const chalk = require('chalk');
 const log = console.log;
 const nunjucks = require('nunjucks');
 var env = nunjucks.configure('views');
-var index = env.render('index.html');
+var locals = {
+  title: 'Fantasy Characters',
+  people: [
+    {name: 'Gandalf'},
+    {name: 'Frodo'},
+    {name: 'Hermione'}
+  ]
+}
+var index = env.render('index.html', locals);
 
 app.use(function (req, res, next) {
   log(chalk.white(req.method, req.url));
@@ -16,8 +24,8 @@ app.listen(3000,() => {
 });
 
 app.get('/', (req, res) => {
-	//res.send(index);
-	res.send(`
+	res.send(index);
+	/*res.send(`
     <html>
 		<head>
 			<title>Homepage</title>
@@ -26,5 +34,5 @@ app.get('/', (req, res) => {
 			<h1>Welcome!</h1>
 		</body>
     </html>
-	`)
+	`)*/
 });
